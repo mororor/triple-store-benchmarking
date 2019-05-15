81c983602c4dd1675cf9e43853d75833bbc2f063
mororor <matthias@syndikat.org>
2019-05-15 13:11:34 +0200

    draft for prepare toolchain
    - download cco (if needed)
    - hyper-graph-db and LevelGraph data ingestion

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const url = require('url')
 const path = require('path')
 const $rdf = require('rdflib')
 const hypergraph = require('hyper-graph-db')
+const levelgraph = require('levelgraph')
+const levelBrowserify = require('level-browserify')
 const Progress = require('progress')
 const md5 = require('md5')
 const sha1 = require('node-sha1')
+const toml = require('toml')
 
 // Links of test data:
 const online_cco = {
@@ -20,22 +23,56 @@ const online_cco_09 = {
   url: 'http://data.bioontology.org/ontologies/CCO/submissions/8/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb'
 }
 
-const online_dw = {
-  name: 'Dat_Whitepaper',
-  url: 'https://github.com/datprotocol/whitepaper/raw/master/dat-paper.pdf'
-}
-
 // directories:
+const configDir = './config.toml'
 const ressourcesDir = './data/ressources'
-const graphDir = './data/graphDBs/hyper-graph-db'
+const graphDir = './data/graphDBs/'
 
 main()
 
 async function main () {
-  const rawData = await getRessources(ressourcesDir, [online_cco])
-  console.log('rawData', typeof rawData[0])
-  const graphDB = await getHypergraph(graphDir)
-  ressourceIngestion(graphDB, rawData[0])
+  const { ressources, graphSelection } = await getConfig()
+  console.log(ressources, graphSelection)
+  const rawData = await getRessources(ressourcesDir, ressources)
+  const graphs = await getGraphs(graphSelection)
+  // const graphDB = await getHypergraph(graphDir)
+  for (graph of Object.keys(graphs)) {
+    // console.log(graph)
+    // console.log(graphs[graph])
+    ressourceIngestion(graphs[graph], rawData[0])
+  }
+}
+
+async function getConfig () {
+  let confToml = await fs.readFileSync(configDir).toString()
+  let { ressources, graphDBs: graphs } = toml.parse(confToml)
+  processedRessources = []
+  for (entry of Object.keys(ressources)) {
+    processedRessources.push(ressources[entry])
+  }
+  return { ressources: processedRessources, graphSelection: graphs }
+}
+
+async function getGraphs (graphSelection) {
+  console.log(graphSelection)
+  for (key of Object.keys(graphSelection)) {
+    if (!graphSelection[key]) continue
+    switch (key) {
+      case 'hyper-graph-db':
+        graphSelection[key] = await getHypergraph(path.join(graphDir, key))
+        break
+      case 'LevelGraph':
+        graphSelection[key] = await getLevelGraph(path.join(graphDir, key))
+        break
+      default:
+        continue
+    }
+  }
+  return graphSelection
+}
+
+async function getLevelGraph (graphDir) {
+  return levelgraph(levelBrowserify(graphDir))
 }
 
 async function getHypergraph (graphDir) {
@@ -43,25 +80,30 @@ async function getHypergraph (graphDir) {
   let key
   try {
     key = await fs.readFileSync(keyFilePath).toString()
-    key = Buffer.from(key, 'hex')
+    console.log('key:', key)
+    if (key !== '') key = Buffer.from(key, 'hex')
+    else key = null
   } catch (err) {
     key = null
   }
-
+  
   if (key) return hypergraph(graphDir, key, { valueEncoding: 'utf-8' })
 
   let newGraphDB = hypergraph(graphDir, { valueEncoding: 'utf-8' })
   newGraphDB.ready(() => {
+    console.log(newGraphDB.db.key.toString('hex'))
     fs.writeFile(keyFilePath,
         newGraphDB.db.key.toString('hex'), (err) => console.warn(err)
       )
   })
-  return graphDB
+  return newGraphDB
 }
 
 async function ressourceIngestion (graphDB, rawData) {
+  await graphDB
+  console.log('graphDB:', graphDB)
   let rdfBlocks = rawData.split('\n\n')
-
+  rdfBlocks.pop()
   let rdfHead = rdfBlocks.shift()
   let rdfTailFragments = rdfBlocks.pop().split('\n')
   let rdfTail = rdfTailFragments.pop()
@@ -74,21 +116,22 @@ async function ressourceIngestion (graphDB, rawData) {
   let baseUrl = "http://data.bioontology.org/ontologies/CCO"
 
   let i = 0
-  let step = 100
+  let step = 1000
   let length = rdfBlocks.length
 
   while (i < length) {
     let j = i + step
     let store = $rdf.graph();
 
-    console.log(i,j,length)
-    if (j > length) {
+    // console.log(i,j,length)
+    if (j >= length) {
       j = length - 1
       if (i===j) break
     }
-    console.log(i,j,length)
+    // console.log(i,j,length)
     
     let parseString = rdfHead + '\n\n' + rdfBlocks.slice(i, j).reduce((sum, elem) => sum + elem + '\n\n') + rdfTail
+    // console.log('parseString', parseString)
     try {
       $rdf.parse(parseString, store, baseUrl, contentType)
     } catch (err) {
@@ -96,10 +139,15 @@ async function ressourceIngestion (graphDB, rawData) {
     }
 
     let stms = store.statementsMatching(undefined, undefined, undefined)
-    console.log('typeof stms:', typeof stms[0], stms[0], stms.length)
+    // console.log('typeof stms:', typeof stms)
+    // console.log('typeof stms[0]:', typeof stms[0])
+    // console.log('stms[0]:', stms[0])
+    // console.log('stms.length:', stms.length)
     let triples = stms.map(elem => ({ subject: elem.subject.value, object: elem.object.value, predicate: elem.predicate.value }))
-    console.log('triples', triples[0])
-    graphDB.put(triples)
+    // console.log('triples[0]', triples[0])
+    // console.log('triples', triples)
+    store.close()
+    await putToGraphAsync(graphDB, triples).catch('error here')
 
     i = j
   }
@@ -117,15 +165,28 @@ async function ressourceIngestion (graphDB, rawData) {
   return
 }
 
-async function getRessources (ressourcesDir, onlineRessources) {
-  const checkSums = await getCheckSums(ressourcesDir)
-  let existingRessources
-  if (checkSums) {
-    existingRessources = await checkFiles(onlineRessources, { checkSums })
-  }
-  for (index in onlineRessources) {
-    if (!existingRessources[index]) {
-      download(ressourcesDir, onlineRessources[index])
+async function putToGraphAsync (graph, triples) {
+  return new Promise((resolve, reject) => graph.put(triples, (err, res) => {
+    if (err) {
+      console.warn(err)
+      reject(err)
+    }
+    resolve(res) 
+  }))
+}
+
+async function getRessources (ressourcesDir, onlineRessources, doNotUpdate) {
+  doNotUpdate = true
+  if (!doNotUpdate) {
+    const checkSums = await getCheckSums(ressourcesDir)
+    let existingRessources
+    if (checkSums) {
+      existingRessources = await checkFiles(onlineRessources, { checkSums })
+    }
+    for (index in onlineRessources) {
+      if (!existingRessources[index]) {
+        download(ressourcesDir, onlineRessources[index])
+      }
     }
   }
   let ret = []
